7c9bd6f036b2bfc47752190067ac9ec6d95f0b3c
Risa Newyear-Ramirez <risapisa@gmail.com>
2015-03-28 13:09:47 -0700

    restaurant dummy data

---
+++ ListOfRestaurants.js
@@ -0,0 +1,12 @@
+{"restaurants";[
+    {"price":"$", "type":"Chinese", "location":"Vancouver, BC"}, 
+    {"price":"$$", "type":"Indian", "location":"Vancouver, BC"},
+    {"price":"$$", "type":"Japanese", "location":"Vancouver, BC"},
+    {"price":"$$$", "type":"Thai", "location":"Vancouver, BC"},
+    {"price":"$", "type":"Pizza", "location":"Toronto, Ontario"},
+    {"price":"$$", "type":"Seafood", "location":"Toronto, Ontario"},
+    {"price":"$$$", "type":"Italian", "location":"Toronto, Ontario"},
+    {"price":"$$", "type":"Mexican", "location":"Montreal, Quebec"},
+    {"price":"$", "type":"Burgers", "location":"Montreal, Quebec"},
+    {"price":"$$$", "type":"Caribbean", "location":"Montreal, Quebec"}
+]}
