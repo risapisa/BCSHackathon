dc319ef8fd9c6cc7c108d2dcfbc0db94a3fe3c66
Risa Newyear-Ramirez <risapisa@gmail.com>
2015-03-28 12:17:50 -0700

    added beginning html pages to call js

--- Main.js
+++ Main.js
@@ -4,3 +4,18 @@
 //Users are logged in based on username/ID, and vote in real time on the three parameters
 //listed above. Then the Yelp API is queried based on their votes and one restaurant
 //is presented.
+
+//First page is setting up login/password for user
+
+/*This Script allows people to enter by using a form that asks for a
+UserID and Password*/
+function pasuser(form) {
+if (form.id.value=="JavaScript") { 
+if (form.pass.value=="Kit") {              
+location="login.html" 
+} else {
+alert("Invalid Password")
+}
+} else {  alert("Invalid UserID")
+}
+}
