c80d3bb97d87ce93cf13c17e568392886ee4fb99
Risa Newyear-Ramirez <risapisa@gmail.com>
2015-03-28 11:37:20 -0700

    Start of project

---
+++ Main.js
@@ -0,0 +1,6 @@
+// Voting app for restaurants for a group of friends based on price range, location,
+// and type of restaurant.
+
+//Users are logged in based on username/ID, and vote in real time on the three parameters
+//listed above. Then the Yelp API is queried based on their votes and one restaurant
+//is presented.
